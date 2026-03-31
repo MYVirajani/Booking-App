9318b9ef8ff365333deb7468aad3e698f5556b02
Yasodha <139672828+MYVirajani@users.noreply.github.com>
2026-03-31 23:49:31 +0530

    welcome screen

---
+++ frontend/lib/presentation/screens/welcome_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.sports_cricket, size: 80),
+            const SizedBox(height: 20),
+            const Text(
+              "Indoor Sports Booking",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Book your favorite playgrounds easily",
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              onPressed: () {
+
+              },
+              child: const Text("Get Started"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
